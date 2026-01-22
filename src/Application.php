3a1d103f86edc3fcf16da12672fe6218042e13f0
José <degraciaj2299@gmail.com>
2026-01-21 19:31:39 -0500

    Middleware JWT cookie → Authorization header

--- src/Application.php
+++ src/Application.php
@@ -3,17 +3,11 @@ declare(strict_types=1);
 
 /**
  * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * Copyright (c) Cake Software Foundation, Inc.
  *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link      https://cakephp.org CakePHP(tm) Project
- * @since     3.3.0
- * @license   https://opensource.org/licenses/mit-license.php MIT License
- */
+  * Licensed under The MIT License
+  */
+
 namespace App;
 
 use Cake\Core\Configure;
@@ -28,25 +22,20 @@ use Cake\ORM\Locator\TableLocator;
 use Cake\Routing\Middleware\AssetMiddleware;
 use Cake\Routing\Middleware\RoutingMiddleware;
 
+/* ✅ AÑADIDO */
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Message\ResponseInterface;
+/* ✅ FIN AÑADIDO */
+
 /**
- * Application setup class.
- *
- * This defines the bootstrapping logic and middleware layers you
- * want to use in your application.
- *
- * @extends \Cake\Http\BaseApplication<\App\Application>
- */
+  * Application setup class.
+  */
 class Application extends BaseApplication
-{
-    /**
-     * Load all the application configuration and bootstrap logic.
-     *
-     * @return void
-     */
+ {
     public function bootstrap(): void
     {
-        // Call parent to load bootstrap from files.
-        parent::bootstrap();
+    parent::bootstrap();
 
         if (PHP_SAPI === 'cli') {
             $this->bootstrapCli();
@@ -57,48 +46,52 @@ class Application extends BaseApplication
             );
         }
 
-        /*
-         * Only try to load DebugKit in development mode
-         * Debug Kit should not be installed on a production system
-         */
-        if (Configure::read('debug')) {
+    if (Configure::read('debug')) {
             $this->addPlugin('DebugKit');
         }
-
-        // Load more plugins here
     }
 
-    /**
-     * Setup the middleware queue your application will use.
-     *
-     * @param \Cake\Http\MiddlewareQueue $middlewareQueue The middleware queue to setup.
-     * @return \Cake\Http\MiddlewareQueue The updated middleware queue.
-     */
     public function middleware(MiddlewareQueue $middlewareQueue): MiddlewareQueue
     {
         $middlewareQueue
-            // Catch any exceptions in the lower layers,
-            // and make an error page/response
+            // Catch any exceptions in the lower layers
             ->add(new ErrorHandlerMiddleware(Configure::read('Error'), $this))
 
-            // Handle plugin/theme assets like CakePHP normally does.
+            /* 🔐 AÑADIDO: JWT Cookie → Authorization Header */
+            ->add(function (
+                ServerRequestInterface $request,
+                RequestHandlerInterface $handler
+            ): ResponseInterface {
+
+                // Leer la cookie jwt_token
+                $cookies = $request->getCookieParams();
+
+                if (!empty($cookies['jwt_token'])) {
+                    $jwt = $cookies['jwt_token'];
+
+                    // Inyectar el token en el header Authorization
+                    $request = $request->withHeader(
+                        'Authorization',
+                        'Bearer ' . $jwt
+                    );
+                }
+
+                return $handler->handle($request);
+            })
+            /* 🔐 FIN AÑADIDO */
+
+            // Handle plugin/theme assets
             ->add(new AssetMiddleware([
                 'cacheTime' => Configure::read('Asset.cacheTime'),
             ]))
 
-            // Add routing middleware.
-            // If you have a large number of routes connected, turning on routes
-            // caching in production could improve performance.
-            // See https://github.com/CakeDC/cakephp-cached-routing
+            // Routing
             ->add(new RoutingMiddleware($this))
 
-            // Parse various types of encoded request bodies so that they are
-            // available as array through $request->getData()
-            // https://book.cakephp.org/4/en/controllers/middleware.html#body-parser-middleware
+            // Body parser
             ->add(new BodyParserMiddleware())
 
-            // Cross Site Request Forgery (CSRF) Protection Middleware
-            // https://book.cakephp.org/4/en/security/csrf.html#cross-site-request-forgery-csrf-middleware
+            // CSRF
             ->add(new CsrfProtectionMiddleware([
                 'httponly' => true,
             ]));
@@ -106,30 +99,13 @@ class Application extends BaseApplication
         return $middlewareQueue;
     }
 
-    /**
-     * Register application container services.
-     *
-     * @param \Cake\Core\ContainerInterface $container The Container to update.
-     * @return void
-     * @link https://book.cakephp.org/4/en/development/dependency-injection.html#dependency-injection
-     */
-    public function services(ContainerInterface $container): void
+public function services(ContainerInterface $container): void
     {
     }
 
-    /**
-     * Bootstrapping for CLI application.
-     *
-     * That is when running commands.
-     *
-     * @return void
-     */
-    protected function bootstrapCli(): void
+protected function bootstrapCli(): void
     {
-        $this->addOptionalPlugin('Bake');
-
-        $this->addPlugin('Migrations');
-
-        // Load more plugins here
+    $this->addOptionalPlugin('Bake');
+    $this->addPlugin('Migrations');
     }
 }
