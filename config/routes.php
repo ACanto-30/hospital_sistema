25991ee706fdf140a7ad22f2d7a2199bfdb0212f
BastienA2005 <basapa13@gmail.com>
2026-01-23 18:19:14 -0500

    Eliminar rutas basura y conexiones directas y login
    Se eliminaron rutas genéricas para evitar accesos no controlados y se definió una ruta explícita para login apuntando al plugin de autenticación

--- config/routes.php
+++ config/routes.php
@@ -26,71 +26,42 @@ use Cake\Routing\RouteBuilder;
 
 /*
  * This file is loaded in the context of the `Application` class.
-  * So you can use  `$this` to reference the application class instance
-  * if required.
+ * So you can use `$this` to reference the application class instance
+ * if required.
  */
 return function (RouteBuilder $routes): void {
-    /*
-     * The default class to use for all routes
-     *
-     * The following route classes are supplied with CakePHP and are appropriate
-     * to set as the default:
-     *
-     * - Route
-     * - InflectedRoute
-     * - DashedRoute
-     *
-     * If no call is made to `Router::defaultRouteClass()`, the class used is
-     * `Route` (`Cake\Routing\Route\Route`)
-     *
-     * Note that `Route` does not do any inflections on URLs which will result in
-     * inconsistently cased URLs when used with `{plugin}`, `{controller}` and
-     * `{action}` markers.
-     */
+
     $routes->setRouteClass(DashedRoute::class);
 
     $routes->scope('/', function (RouteBuilder $builder): void {
-        /*
-         * Here, we are connecting '/' (base path) to a controller called 'Pages',
-         * its action called 'display', and we pass a param to select the view file
-         * to use (in this case, templates/Pages/home.php)...
-         */
-        $builder->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
 
         /*
-         * ...and connect the rest of 'Pages' controller's URLs.
+         * Página principal
          */
-        $builder->connect('/pages/*', 'Pages::display');
+        $builder->connect(
+            '/',
+            ['controller' => 'Pages', 'action' => 'display', 'home']
+        );
 
         /*
-         * Connect catchall routes for all controllers.
-         *
-         * The `fallbacks` method is a shortcut for
-         *
-         * ```
-         * $builder->connect('/{controller}', ['action' => 'index']);
-         * $builder->connect('/{controller}/{action}/*', []);
-         * ```
-         *
-         * You can remove these routes once you've connected the
-         * routes you want in your application.
+         * LOGIN – ruta explícita apuntando al PLUGIN Authentication
          */
-        $builder->fallbacks();
+        $builder->connect(
+            '/login',
+            [
+                'plugin' => 'Authentication',
+                'controller' => 'Users',
+                'action' => 'login'
+            ]
+        );
+
+        /*
+         *   RUTAS BASURA ELIMINADAS
+         * - Accesos genéricos sin control
+         * - Riesgo de seguridad con Authentication
+         */
+
+        // $builder->connect('/pages/*', 'Pages::display');
+        // $builder->fallbacks();
     });
-
-    /*
-     * If you need a different set of middleware or none at all,
-     * open new scope and define routes there.
-     *
-     * ```
-     * $routes->scope('/api', function (RouteBuilder $builder): void {
-     *     // No $builder->applyMiddleware() here.
-     *
-     *     // Parse specified extensions from URLs
-     *     // $builder->setExtensions(['json', 'xml']);
-     *
-     *     // Connect API actions here.
-     * });
-     * ```
-     */
 };
